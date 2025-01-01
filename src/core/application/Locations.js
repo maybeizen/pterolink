eebf1b658bcc74fa87e63af47ee8d02d8d00208f
Briar Chamberlin <izenthedev@gmail.com>
2025-01-01 16:35:08 -0500

    JSDoc intellisense for Locations.js and Nests.js

--- src/core/application/Locations.js
+++ src/core/application/Locations.js
@@ -1,8 +1,22 @@
+/**
+ * Represents the Locations API endpoint handler
+ * @class
+ */
 class Locations {
+  /**
+   * Creates a new Locations instance
+   * @param {import('../Client.js').default} client - The ApplicationClient instance
+   */
   constructor(client) {
     this.client = client;
   }
 
+  /**
+   * List all locations on the panel
+   * @returns {Promise<Array<Object>>} Array of location objects
+   * @throws {UnauthorizedError} If the API key is invalid
+   * @throws {PteroError} If the API request fails
+   */
   async list() {
     try {
       const response = await this.client.axios.get("/locations");
@@ -12,6 +26,14 @@ class Locations {
     }
   }
 
+  /**
+   * Get details of a specific location
+   * @param {number|string} id - The ID of the location
+   * @returns {Promise<Object>} Location details
+   * @throws {NotFoundError} If the location doesn't exist
+   * @throws {UnauthorizedError} If the API key is invalid
+   * @throws {PteroError} If the API request fails
+   */
   async get(id) {
     try {
       const response = await this.client.axios.get(`/locations/${id}`);
@@ -21,6 +43,16 @@ class Locations {
     }
   }
 
+  /**
+   * Create a new location
+   * @param {Object} data - The location creation data
+   * @param {string} data.short - Short identifier for the location
+   * @param {string} data.long - Long name for the location
+   * @returns {Promise<Object>} Created location details
+   * @throws {ValidationError} If the provided data is invalid
+   * @throws {UnauthorizedError} If the API key is invalid
+   * @throws {PteroError} If the API request fails
+   */
   async create(data) {
     try {
       const response = await this.client.axios.post("/locations", data);
@@ -30,6 +62,18 @@ class Locations {
     }
   }
 
+  /**
+   * Update a location's details
+   * @param {number|string} id - The ID of the location to update
+   * @param {Object} data - The data to update
+   * @param {string} [data.short] - Short identifier for the location
+   * @param {string} [data.long] - Long name for the location
+   * @returns {Promise<Object>} Updated location details
+   * @throws {NotFoundError} If the location doesn't exist
+   * @throws {ValidationError} If the provided data is invalid
+   * @throws {UnauthorizedError} If the API key is invalid
+   * @throws {PteroError} If the API request fails
+   */
   async update(id, data) {
     try {
       const response = await this.client.axios.patch(`/locations/${id}`, data);
@@ -39,6 +83,14 @@ class Locations {
     }
   }
 
+  /**
+   * Delete a location
+   * @param {number|string} id - The ID of the location to delete
+   * @returns {Promise<boolean>} True if the location was deleted successfully
+   * @throws {NotFoundError} If the location doesn't exist
+   * @throws {UnauthorizedError} If the API key is invalid
+   * @throws {PteroError} If the API request fails
+   */
   async delete(id) {
     try {
       await this.client.axios.delete(`/locations/${id}`);
--- src/core/application/Nests.js
+++ src/core/application/Nests.js
@@ -1,9 +1,23 @@
+/**
+ * Represents the Nests API endpoint handler
+ * @class
+ */
 class Nests {
+  /**
+   * Creates a new Nests instance
+   * @param {import('../Client.js').default} client - The ApplicationClient instance
+   */
   constructor(client) {
     this.client = client;
     this.eggs = new Eggs(this.client);
   }
 
+  /**
+   * List all nests on the panel
+   * @returns {Promise<Array<Object>>} Array of nest objects
+   * @throws {UnauthorizedError} If the API key is invalid
+   * @throws {PteroError} If the API request fails
+   */
   async list() {
     try {
       const response = await this.client.axios.get("/nests");
@@ -13,6 +27,14 @@ class Nests {
     }
   }
 
+  /**
+   * Get details of a specific nest
+   * @param {number|string} id - The ID of the nest
+   * @returns {Promise<Object>} Nest details
+   * @throws {NotFoundError} If the nest doesn't exist
+   * @throws {UnauthorizedError} If the API key is invalid
+   * @throws {PteroError} If the API request fails
+   */
   async get(id) {
     try {
       const response = await this.client.axios.get(`/nests/${id}`);
@@ -23,11 +45,27 @@ class Nests {
   }
 }
 
+/**
+ * Represents the Eggs API endpoint handler
+ * @class
+ */
 class Eggs {
+  /**
+   * Creates a new Eggs instance
+   * @param {import('../Client.js').default} client - The ApplicationClient instance
+   */
   constructor(client) {
     this.client = client;
   }
 
+  /**
+   * List all eggs in a nest
+   * @param {number|string} id - The ID of the nest
+   * @returns {Promise<Array<Object>>} Array of egg objects
+   * @throws {NotFoundError} If the nest doesn't exist
+   * @throws {UnauthorizedError} If the API key is invalid
+   * @throws {PteroError} If the API request fails
+   */
   async list(id) {
     try {
       const response = await this.client.axios.get(`/nests/${id}/eggs`);
@@ -37,6 +75,15 @@ class Eggs {
     }
   }
 
+  /**
+   * Get details of a specific egg
+   * @param {number|string} id - The ID of the nest
+   * @param {number|string} eggId - The ID of the egg
+   * @returns {Promise<Object>} Egg details
+   * @throws {NotFoundError} If the nest or egg doesn't exist
+   * @throws {UnauthorizedError} If the API key is invalid
+   * @throws {PteroError} If the API request fails
+   */
   async get(id, eggId) {
     try {
       const response = await this.client.axios.get(
